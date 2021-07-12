72a46393e0f090666a6d696e5443c89036966203
Harithya Wisesa <harithya77@gmail.com>
2021-07-12 15:44:28 +0700

    menambahkan perhitungan korelasi dan juga tab

--- app/Http/Controllers/KriptoController.php
+++ app/Http/Controllers/KriptoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use MathPHP\Statistics\Correlation;
 
 class KriptoController extends Controller
 {
@@ -22,33 +23,9 @@ class KriptoController extends Controller
     {
         $tmp = [];
         foreach ($text as $key => $value) {
-            $tmp[$key]['value'] = array_search(strtolower($value), $this->abjad);
+            $tmp[$key] = array_search(strtolower($value), $this->abjad);
         }
         return $tmp;
-    }
-
-    public function calculateCorelation($x, $y)
-    {
-        if (count($x) !== count($y)) {
-            return -1;
-        }
-        $x = array_values($x);
-        $y = array_values($y);
-        $xs = array_sum($x) / count($x);
-        $ys = array_sum($y) / count($y);
-        $a = 0;
-        $bx = 0;
-        $by = 0;
-        for ($i = 0; $i < count($x); $i++) {
-            $xr = $x[$i] - $xs;
-            $yr = $y[$i] - $ys;
-            $a += $xr * $yr;
-            $bx += pow($xr, 2);
-            $by += pow($yr, 2);
-        }
-        $b = sqrt($bx * $by);
-        if ($b == 0) return 0;
-        return $a / $b;
     }
 
     public function store(Request $request)
@@ -61,8 +38,7 @@ class KriptoController extends Controller
             'chipperText' => $this->calculateChar($chipperText)
         ];
 
-        dd($this->calculateCorelation($result['plainText'], $result['chipperText']));
-
+        // dd(Correlation::r($result['plainText'], $result['chipperText']));
         return view('result', compact('result'));
     }
 }
--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -3,19 +3,64 @@
 @section('content')
 
     <div class="container mt-5">
-        <table class="table table-bordered">
-            <tr>
-                <th>No</th>
-                <th>X</th>
-                <th>Y</th>
-            </tr>
-            @for ($i = 0; $i < max(count($result['plainText']), count($result['chipperText'])); $i++)
-                <tr>
-                    <td>{{ $i + 1 }}</td>
-                    <td>{{ $result['plainText'][$i]['value'] }}</td>
-                    <td>{{ $result['chipperText'][$i]['value'] }}</td>
-                </tr>
-            @endfor
-        </table>
+        <ul class="nav nav-tabs" id="myTab" role="tablist">
+            <li class="nav-item" role="presentation">
+                <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button"
+                    role="tab" aria-controls="home" aria-selected="true">Uji Korelasi</button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact" type="button"
+                    role="tab" aria-controls="contact" aria-selected="false">Analisis Histogram</button>
+            </li>
+        </ul>
+        <div class="tab-content " id="myTabContent">
+            <div class="tab-pane fade  show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                <table class="table table-bordered mt-5">
+                    <tr>
+                        <th>No</th>
+                        <th>X</th>
+                        <th>Y</th>
+                        <th>X<sup>2</sup></th>
+                        <th>Y<sup>2</sup></th>
+                        <th>X*Y</th>
+                    </tr>
+                    @php
+                        $x = 0;
+                        $y = 0;
+                        $x2 = 0;
+                        $y2 = 0;
+                        $xy = 0;
+                    @endphp
+                    @for ($i = 0; $i < max(count($result['plainText']), count($result['chipperText'])); $i++)
+                        <tr>
+                            <td>{{ $i + 1 }}</td>
+                            <td>{{ $result['plainText'][$i] ?? '-' }}</td>
+                            <td>{{ $result['chipperText'][$i] ?? '-' }}</td>
+                            <td>{{ pow($result['plainText'][$i] ?? 0, 2) }}</td>
+                            <td>{{ pow($result['chipperText'][$i] ?? 0, 2) }}</td>
+                            <td>{{ ($result['plainText'][$i] ?? 0) * ($result['chipperText'][$i] ?? 0) }}</td>
+                            @php
+                                $x += $result['plainText'][$i] ?? 0;
+                                $y += $result['chipperText'][$i] ?? 0;
+                                $x2 += pow($result['plainText'][$i] ?? 0, 2);
+                                $y2 += pow($result['chipperText'][$i] ?? 0, 2);
+                                $xy += ($result['plainText'][$i] ?? 0) * ($result['chipperText'][$i] ?? 0);
+                            @endphp
+                        </tr>
+                    @endfor
+                    <tr>
+                        <th>Jumlah</th>
+                        <th>{{ $x }}</th>
+                        <th>{{ $y }}</th>
+                        <th>{{ $x2 }}</th>
+                        <th>{{ $y2 }}</th>
+                        <th>{{ $xy }}</th>
+                    </tr>
+                </table>
+            </div>
+            <div class="tab-pane fade mb-5" id="contact" role="tabpanel" aria-labelledby="contact-tab">
+
+            </div>
+        </div>
     </div>
 @endsection
